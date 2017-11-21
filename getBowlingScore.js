89df27ffc3fce05fc290be7b70aaaa0738ac08dc
Nathan Leonard <nleonard@ha003929.wvrgroup.internal>
2017-11-20 19:51:50 -0500

    implement basic scoring

--- getBowlingScore.js
+++ getBowlingScore.js
@@ -1,1 +1,11 @@
-module.exports = () => {};
+module.exports = (scoreString) => {
+  const scores = scoreString.split(' ');
+  return scores.reduce((total, frameString) => {
+    const frame = frameString.split('');
+    const frameScore = frame.reduce((frameTotal, throwString) => {
+      const throwScore = parseInt(throwString, 10);
+      return frameTotal + throwScore;
+    }, 0);
+    return total + frameScore;
+  }, 0);
+};

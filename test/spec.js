1d488faf5be4f608e0faf22a520dbf845d448dfe
Nathan Leonard <nleonard@ha003929.wvrgroup.internal>
2017-11-20 19:35:56 -0500

    create v1 specs

--- test/spec.js
+++ test/spec.js
@@ -1,16 +1,33 @@
 const { expect } = require('chai');
 const getBowlingScore = require('../getBowlingScore');
 
-// console.log(getBowlingScore);
-// const PERFECT_GAME = 'X X X X X X X X X X X X';
-// const ALL_NINES = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-';
-// const WITH_SPARES = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5';
-
 describe('getBowlingScore v1', () => {
   it('should be a function', () => {
     expect(typeof getBowlingScore).to.equal('function');
   });
 
+  it('can calculate a basic game', () => {
+    const BASIC_GAME = '12 34 12 34 12 34 12 34 12 34'; // 50
+    expect(getBowlingScore(BASIC_GAME)).to.equal(50);
+  });
+
   it('can calculate a perfect game', () => {
+    const PERFECT_GAME = 'X X X X X X X X X X X X'; // 300
+    expect(getBowlingScore(PERFECT_GAME)).to.equal(300);
+  });
+
+  it('can calculate a game with misses', () => {
+    const WITH_MISSES = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-'; // 90
+    expect(getBowlingScore(WITH_MISSES).to.equal(90));
+  });
+
+  it('can calculate a game with spares', () => {
+    const WITH_SPARES = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5'; // 150
+    expect(getBowlingScore(WITH_SPARES).to.equal(150));
+  });
+
+  it('can calculate a game with all types', () => {
+    const TEST_GAME = '12 3- 4/ 51 X 72 8- X -9 3/-'; // 101
+    expect(getBowlingScore(TEST_GAME)).to.equal(101);
   });
 });

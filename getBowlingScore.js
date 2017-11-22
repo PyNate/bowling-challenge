df0463e5adcc45d07cadad8a40ebab65c718a73d
Nathan Leonard <nleonard@ha003929.wvrgroup.internal>
2017-11-22 13:21:11 -0500

    handle spares

--- getBowlingScore.js
+++ getBowlingScore.js
@@ -4,21 +4,49 @@ const MISS = '-';
 
 function getBowlingScore(scoreString) {
   const scores = scoreString.split(' ');
+  let doubledThrows = 0;
   return scores.reduce((total, frameString) => {
-    const frameScore = getSingleFrameScore(frameString);
+    let frameScore;
+    if (doubledThrows > 1) {
+      frameScore = getSingleFrameScore(frameString, true, true);
+      doubledThrows -= 2;
+    } else if (doubledThrows === 1) {
+      frameScore = getSingleFrameScore(frameString, true);
+      doubledThrows -= 1;
+    } else {
+      frameScore = getSingleFrameScore(frameString);
+    }
+
+    if (isSpare(frameString)) {
+      doubledThrows += 1;
+    }
     return total + frameScore;
   }, 0);
 }
 
-function getSingleFrameScore(frameString) {
-  if (isStrike(frameString) || isSpare(frameString)) {
+function getSingleFrameScore(frameString, doubleFirstThrow, doubleSecondThrow) {
+  if (isStrike(frameString)) {
     return 10;
   }
-  return frameString.split('').reduce((total, throwString) => {
-    if (throwString === MISS) {
-      return total;
+  let frameTotal = 0;
+  return frameString.split('').reduce((total, throwString, index) => {
+    let throwScore;
+    switch (throwString) {
+      case (SPARE):
+        throwScore = 10 - frameTotal;
+        break;
+      case (MISS):
+        throwScore = 0;
+        break;
+      default:
+        throwScore = parseInt(throwString, 10);
+        frameTotal += throwScore;
+        break;
     }
-    return total + parseInt(throwString, 10);
+    if ((doubleFirstThrow && index === 0) || (doubleSecondThrow && index === 1)) {
+      return total + (2 * throwScore);
+    }
+    return total + throwScore;
   }, 0);
 }
 

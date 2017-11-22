3b95aa8d32da5ff8ab7ed1e29b393cc8ee14addf
Nathan Leonard <nleonard@ha003929.wvrgroup.internal>
2017-11-22 15:17:03 -0500

    add validation test cases

--- test/spec.js
+++ test/spec.js
@@ -1,43 +1,144 @@
 const { expect } = require('chai');
 const getBowlingScore = require('../getBowlingScore');
 
-describe('getBowlingScore v1', () => {
+describe('getBowlingScore', () => {
   it('should be a function', () => {
     expect(typeof getBowlingScore).to.equal('function');
   });
 
   it('can calculate a basic game', () => {
-    const BASIC_GAME = '12 34 12 34 12 34 12 34 12 34'; // 50
-    expect(getBowlingScore(BASIC_GAME)).to.equal(50);
+    const testGame = '12 34 12 34 12 34 12 34 12 34'; // 50
+    expect(getBowlingScore(testGame)).to.equal(50);
   });
 
   it('can calculate a perfect game', () => {
-    const PERFECT_GAME = 'X X X X X X X X X X X X'; // 300
-    expect(getBowlingScore(PERFECT_GAME)).to.equal(300);
+    const testGame = 'X X X X X X X X X X X X'; // 300
+    expect(getBowlingScore(testGame)).to.equal(300);
   });
 
   it('can calculate a game with misses', () => {
-    const WITH_MISSES = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-'; // 90
-    expect(getBowlingScore(WITH_MISSES)).to.equal(90);
+    const testGame = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9-'; // 90
+    expect(getBowlingScore(testGame)).to.equal(90);
   });
 
   it('can calculate a game with spares', () => {
-    const WITH_SPARES = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5'; // 150
-    expect(getBowlingScore(WITH_SPARES)).to.equal(150);
+    const testGame = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5'; // 150
+    expect(getBowlingScore(testGame)).to.equal(150);
   });
 
   it('can calculate a game with all types', () => {
-    const TEST_GAME = '12 3- 4/ 51 X 72 8- X -9 3/-'; // 101
-    expect(getBowlingScore(TEST_GAME)).to.equal(101);
+    const testGame = '12 3- 4/ 51 X 72 8- X -9 3/-'; // 101
+    expect(getBowlingScore(testGame)).to.equal(101);
   });
 
   it('can calculate a strike in the 10th frame', () => {
-    const TEST_GAME_2 = '12 3- 4/ 51 X 72 8- X -9 X 9/'; // 111
-    expect(getBowlingScore(TEST_GAME_2)).to.equal(111);
+    const testGame = '12 3- 4/ 51 X 72 8- X -9 X 9/'; // 111
+    expect(getBowlingScore(testGame)).to.equal(111);
   });
 
   it('can calculate two strikes in the 10th frame', () => {
-    const ALMOST_PERFECT_GAME = 'X X X X X X X X X X X 9'; // 299
-    expect(getBowlingScore(ALMOST_PERFECT_GAME)).to.equal(299);
+    const testGame = 'X X X X X X X X X X X 9'; // 299
+    expect(getBowlingScore(testGame)).to.equal(299);
+  });
+});
+
+describe('getBowlingScore validation', () => {
+  it('throws an incomplete game error if there are too few frames', () => {
+    const testGame = '9- 9- 9- 9- 9- 9-';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('incomplete game');
+  });
+
+  it('throws an incomplete game error if bonus throws are missing', () => {
+    const testGame = 'X X X X X X X X X X X';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('incomplete game');
+  });
+
+  it('throws an invalid game error if too many frames are submitted', () => {
+    const testGame = 'X X X X X X X X X X X X X';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid game');
+  });
+
+  it('throws an invalid game error if a character is invalid', () => {
+    const testGame = 'X A X X X X X X X X X X';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid game');
+  });
+
+  it('throws an invalid frame error if a frame is incomplete', () => {
+    const testGame = '9- 9- 9- 9- 9 9- 9- 9- 9- 9-';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid frame (5)');
+  });
+
+  it('throws an invalid frame error if a frame counts too many pins', () => {
+    const testGame = '9- 9- 9- 9- 98 9- 9- 9- 9- 9-';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid frame (5)');
+  });
+
+  it('throws an invalid frame error if a bonus spare throw is missing', () => {
+    const testGame = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid frame (10)');
+  });
+
+  it('throws an invalid frame error if a spare is recorded for the first throw', () => {
+    const testGame = '5/ 5/ 5/ /5 5/ 5/ 5/ 5/ 5/ 5/5';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid frame (4)');
+  });
+
+  it('throws an invalid frame error if a frame has too many throws', () => {
+    const testGame = '5/ 5/ 5/ 5/ 5/ 5/5 5/ 5/ 5/ 5/5';
+    let testError;
+    try {
+      getBowlingScore(testGame);
+    } catch (e) {
+      testError = e;
+    }
+    expect(testError.message).to.equal('invalid frame (6)');
   });
 });
